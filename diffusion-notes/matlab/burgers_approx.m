d9902a06c04098f663b155722645fb0ee023e9f1
Tony Roberts <anthony.roberts@adelaide.edu.au>
2015-01-09 14:17:10 +1030

    burgers/matlab/approx integration and error detection

--- diffusion-notes/matlab/burgers_approx.m
+++ diffusion-notes/matlab/burgers_approx.m
@@ -5,9 +5,9 @@ function burgers_approx
 % Use finite difference approximations for u_x and u_xx.
 %----------------------------------------------------------------
 % Set up domain:
-global L x H j
-L=11;                 % Periodicity
-x=linspace(0,1,L+1)'; % Grid-points
+global L x H j te ye ie
+L=19;                 % Periodicity
+x=linspace(0,2*pi,L+1)'; % Grid-points
 H=x(2)-x(1);          % Grid-point spacing
 j=1:L;                % Grid-point indices, j=0 equiv. j=L
 % Set up operator mu*delta (centred difference) as a matrix:
@@ -31,15 +31,21 @@ S=inv(eye(L,L)+d2/6);
 % Solve PDE from t=0 to t=T:
 global A
 A = 10;
-T=0.5;
-[t,u]=ode15s(@dudt,[0 T],u0());
+T=2;
+if 0, [t,u]=ode15s(@dudt,[0 T],u0());
+else opts=odeset('Events',@largey);
+[t,u,te,ye,ie]=ode15s(@dudt,[0 T],u0(),opts);
+te=te
+ye=ye
+ie=ie
+end
 u=[u(:,L) u]; % Add j=0 point from j=L
 surf(x,t,u);
 %----------------------------------------------------------------
 % Initialisation of u(x,t) at t=0:
 function u=u0()
 global A L x
-u=A*sin(2*pi*x(2:L+1));
+u=A*sin(x(2:L+1));
 %----------------------------------------------------------------
 % Temporal derivative of u(x,t):
 function u_t=dudt(t,u)
@@ -49,3 +55,7 @@ global H d2 md
 u_t=d2*u/H^2-u.*(md*u/H);
 display(u_t)
 %----------------------------------------------------------------
+function [val,isfin,dirn]=largey(t,y)
+isfin=1;
+val=-max(abs(y))+1e3;
+dirn=0;
