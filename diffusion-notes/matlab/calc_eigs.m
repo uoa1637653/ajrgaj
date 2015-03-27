26658e4ed35bc9c8ad2807fc3fc8e33ac497f589
uoa1637653 <geoff.jarrad@hotmail.com.au>
2015-03-27 13:02:20 +1030

    Added REDUCE code for higher epsilon terms, but epsilon^2 still looks horrible - need to investigate further.

--- diffusion-notes/matlab/calc_eigs.m
+++ diffusion-notes/matlab/calc_eigs.m
@@ -6,7 +6,7 @@ function [vec,lam]=calc_eigs(u0,dudt)
 % by init_domain()).
 %----------------------------------------------------------------
 % Find eigenvalues/vectors by perturbing u0 at t=0:
-small=1e-6;
+small=1e-7;
 fun=@(v) (dudt(0,u0+small*v)-dudt(0,u0))/small;
 global L;
 % Note: Ignore error generated (randomly) by complex values.
