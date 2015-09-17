5d3994616c920917b546c4997261f879489ddff8
uoa1637653 <geoff.jarrad@hotmail.com.au>
2015-09-17 10:28:53 +0930

    Added numerical results (without explanation).

--- diffusion-notes/matlab/burgers_stab_plot2.m
+++ diffusion-notes/matlab/burgers_stab_plot2.m
@@ -1,26 +1,37 @@
-function burgers_stab_plot2(Ls)
+function burgers_stab_plot2(M)
 %----------------------------------------------------------------
-% GAJ 17/03/2015
+% GAJ 15/09/2015
 %----------------------------------------------------------------
     figure
     hold on
-    plot_it('adv', Ls,[1 0 0]*0.7); 
-    %plot_it('cons', Ls,[1 1 0]*0.7);
-    %plot_it('mix', Ls,[0 1 0]*0.7);
-    %plot_it('hol', Ls,[0 1 1]*0.7);
+    plot_it('adv', [3,5:M], [1 0 0]*0.7); 
+    plot_it('cons', 6:M, [1 1 0]*0.7);
+    plot_it('mix', 5:M, [0 1 0]*0.7);
+    plot_it('hol', 5:M, [0 1 1]*0.7);
     xlabel('#intervals, L')
     ylabel('critical amplitude, A')
-    %legend('advective','conservative','mixture','holistic')
-    legend('advective')
+    legend('advective','conservative','mixture','holistic')
     hold off
 end
 
 function plot_it(selection,Ls,rgb)
     N = length(Ls);
     As = zeros(1, N);
+    Ts = zeros(1, N);
+    ies = zeros(1, N);
     for i = 1:N
-        [Acrit] = burgers_eigs(selection, Ls(i));
+        [Acrit, Tcrit, ie] = burgers_stability(selection, Ls(i));
         As(i) = Acrit;
+        Ts(i) = Tcrit;
+        ies(i) = ie;
+    if ie == 1
+        sym = '+-';
+    elseif ie == 2
+        sym = 'x-';
+    else
+        sym = 'o-';
     end
-    plot(Ls, As, 'o-', 'Color', rgb);
+        plot(Ls(i), Acrit, sym, 'Color', rgb);
+    end
+    plot(Ls, As, '-', 'Color', rgb);
 end
