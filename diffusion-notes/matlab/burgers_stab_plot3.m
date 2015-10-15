9d9f98cbb73f65c61791711a971a825719901144
uoa1637653 <geoff.jarrad@hotmail.com.au>
2015-10-15 11:57:54 +1030

    Inserted revamped stability plot. Now need to explain results.

--- diffusion-notes/matlab/burgers_stab_plot3.m
+++ diffusion-notes/matlab/burgers_stab_plot3.m
@@ -4,10 +4,10 @@ function burgers_stab_plot3(M)
 %----------------------------------------------------------------
     figure
     hold on
-    p1 = plot_it('adv', 3:M, [1 0.1 0.1]*0.7); 
-    p2 = plot_it('cons', 3:M, [1 1 0.1]*0.7);
-    p3 = plot_it('mix', 3:M, [0.1 1 0.1]*0.7);
-    p4 = plot_it('hol', 3:M, [0.1 1 1]*0.7);
+    p1 = plot_it('adv', 3:M, [1 0.1 0.1]); 
+    p2 = plot_it('cons', 3:M, [0.1 1 0.1]);
+    p3 = plot_it('mix', 3:M, [0.1 0.1 1]);
+    p4 = plot_it('hol', 3:M, [1 1 0.1]*0.7);
     xlabel('number of intervals, N')
     ylabel('critical amplitude, A')
     legend([p1,p2,p3,p4],'advective','conservative','mixture','holistic')
